98bd0fb4ea62a36428d222d727e26d05002f9ba2
jmt1423 <jon.thomann30@gmail.com>
2021-07-27 13:57:12 +0100

    update batch size

--- models/train.py
+++ models/train.py
@@ -22,7 +22,7 @@ Image.MAX_IMAGE_PIXELS = 400000000
 # Hyperparameters
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 16
+BATCH_SIZE = 1
 NUM_EPOCHS = 3
 NUM_WORKERS = 2
 IMAGE_HEIGHT = 512
