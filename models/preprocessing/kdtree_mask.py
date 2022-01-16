9d06a1370f34b6e9a1b35392bb32cb2b2bf2dc82
jmt1423 <jon.thomann30@gmail.com>
2022-01-16 14:15:17 +0000

    kdtree works successfully, removed api keys from github

--- models/preprocessing/kdtree_mask.py
+++ models/preprocessing/kdtree_mask.py
@@ -1,6 +1,11 @@
+"""
+This script takes a masked image from labelbox's API (where I labelled the original image)
+and uses a KDTree algorithm from scipy to threshold each pixel into a given colorspace.
+"""
 from PIL import Image
 import torch
 import time
+import config
 
 import numpy as np
 import matplotlib
@@ -21,15 +26,15 @@ from matplotlib import colors
 from scipy.spatial import cKDTree as KDTree
 from scipy.misc import face
 
-#img = Image.open('../../../CoastSat/data/blackpool/images/bp_labels.jpg').convert('1')
-#img.save('../../../CoastSat/data/blackpool/images/label_greyscale.png')
-# Pick a project that has and of box, point, polygon, or segmentation tools tools in the ontology
-# and has completed labels
-PROJECT_ID = "ckybon3yzbrb30zai53rbfhic"
-# Only update this if you have an on-prem deployment
+
+
+# Project ID of blackpool labels
+PROJECT_ID = config.PROJECT_ID
+# endpoint of labelbox api
 ENDPOINT = "https://api.labelbox.com/graphql"
 
-client = Client(api_key='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJja3lib2o1dmtibTIwMHphOTY1ajhjdjE2Iiwib3JnYW5pemF0aW9uSWQiOiJja3lib2o1dmNibTF6MHphOWZkbzg1M2MzIiwiYXBpS2V5SWQiOiJja3lkM2cxNnc4czhkMHo3dDFjdHNmZ2JzIiwic2VjcmV0IjoiY2RiOTU3OWFjYjIzMTQ4Yjc4MGU4ZWQzOWIxODE2M2QiLCJpYXQiOjE2NDIwODU3MjgsImV4cCI6MjI3MzIzNzcyOH0.tZQkFFY2wa3v_ia6dnDGzDPnyDAIJtrGkGr12h6AFJQ', endpoint=ENDPOINT)
+# client key this needs to be removed and 
+client = Client(api_key=config.LB_API_KEY, endpoint=ENDPOINT)
 project = client.get_project('ckybon3yzbrb30zai53rbfhic')
 
 labels = project.label_generator()
@@ -47,16 +52,15 @@ for annotation in label.annotations:
     if isinstance(annotation.value, Geometry):
         image_np = annotation.value.draw(canvas = image_np, color = colors1[annotation.name], thickness = 5)
 final = Image.fromarray(image_np.astype(np.uint8))
-#final.show()
+
 
 
 # using kdtree to reduce color space of mask image to create correct color mapping for model
-
 REDUCED_COLOR_SPACE = True
 
 # borrow a list of named colors from matplotlib
 if REDUCED_COLOR_SPACE:
-    use_colors = {k: colors.cnames[k] for k in ['red', 'green', 'blue', 'yellow', 'pink', 'purple']}
+    use_colors = {k: colors.cnames[k] for k in ['red', 'blue', 'yellow', 'pink', 'purple', 'green']}
 else:
     use_colors = colors.cnames
 
@@ -67,9 +71,9 @@ ncol = len(named_colors)
 
 if REDUCED_COLOR_SPACE:
     ncol -= 1
-    no_match = named_colors.pop('purple')
+    no_match = named_colors.pop('green')
 else:
-    no_match = named_colors['purple']
+    no_match = named_colors['green']
 
 # make an array containing the RGB values 
 color_tuples = list(named_colors.values())
@@ -79,25 +83,23 @@ color_tuples = np.array(color_tuples)
 color_names = list(named_colors)
 color_names.append('no match')
 
-# get example picture
+
+# get picture
 img = final
 
 # build tree
 tree = KDTree(color_tuples[:-1])
-# tolerance for color match `inf` means use best match no matter how
-# bad it may be
+
+# tolerance for color match `inf` means use best match no matter how bad it is
 tolerance = np.inf
+
 # find closest color in tree for each pixel in picture
 dist, idx = tree.query(img, distance_upper_bound=tolerance)
+
 # count and reattach names
 counts = dict(zip(color_names, np.bincount(idx.ravel(), None, ncol+1)))
 
 print(counts)
 
-import pylab
-
-pylab.imshow(img)
-pylab.savefig('orig.png')
-pylab.clf()
-pylab.imshow(color_tuples[idx])
-pylab.savefig('minimal.png' if REDUCED_COLOR_SPACE else 'reduced.png')
+PIL_image = Image.fromarray(np.uint8(color_tuples[idx])).convert('RGB')
+PIL_image.show()
